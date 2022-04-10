599689a374cdd1b93848ed5e9e02b8ee39a7e6df
Choeurtis <choeurtis.tchounga@gmail.com>
2022-04-10 18:37:34 +0200

    return changes

--- blog-react-php/src/App.tsx
+++ blog-react-php/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Inscription from './Component/Inscription';
 import './App.css';
 import Home from './Component/Home';
+import Post from './Component/Post';
 
 function getCookie(cname: string) {
   let name = cname + "=";
@@ -22,19 +23,57 @@ function getCookie(cname: string) {
 function App() {
   const [id, setID] = useState<Object>({}); 
   const [error, setError] = useState<string>('');  
+  var [posts, setPosts] = useState({});
+
+  useEffect( () => {
+    const headers = new Headers({ 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    fetch('http://localhost:2345/Controller/Post.php', {
+      method: 'POST',
+      headers: headers,
+      mode: 'cors',
+      credentials: 'include'
+    }) 
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setPosts(data); 
+      }
+    )  
+  }, [])
+
+
 
   if(getCookie('token') && getCookie('token') != "") {
     return (
+      <>
       <Home username={getCookie('username')}></Home>
+      {posts.post.map((element: { id: any; }) => (
+          <Post key={element.id} props={{element}}/>
+      ))}
+      </>
     )
   } 
-  return (
-    <div className="App">
-      <Inscription setID={setID} setError={setError}></Inscription>
-      
-      <p className='error-msg'>{error}</p>
-    </div>
-  ) 
+  if(posts.post) {
+    return (
+      <div className="App">
+        <Inscription setID={setID} setError={setError}></Inscription>
+        <p className='error-msg'>{error}</p>
+          {posts.post.map((element: { id: any; }) => (
+              <Post key={element.id} props={{element}}/>
+          ))}
+      </div>
+    ) 
+  } else {
+    return (
+      <div className="App">
+        <Inscription setID={setID} setError={setError}></Inscription>
+      </div>
+    ) 
+  }
 }   
 
 export default App;
--- blog-react-php/src/Component/Home.tsx
+++ blog-react-php/src/Component/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Post from './Post';
 
 interface FormPropsInterface {
     setID: React.Dispatch<any>,
@@ -7,58 +6,21 @@ interface FormPropsInterface {
 }
 
 export default function Home({username}:any) {
-  var [posts, setPosts] = useState({});
   
-  useEffect( () => {
-    const headers = new Headers({ 
-      'Content-Type': 'application/x-www-form-urlencoded'
-    });
-
-    fetch('http://localhost:2345/Controller/Post.php', {
-      method: 'POST',
-      headers: headers,
-      mode: 'cors',
-      credentials: 'include'
-    }) 
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        setPosts(data); 
-      }
-    )  
-  }, [])
-
-
 
   const handleDeco = (e: any) => {
     document.cookie = `token=;`; 
     location.reload();
   }
 
-  if(posts.post) {
-    return (
-      <div>
-        <h1>Hello {username}</h1>
-        <button className="deco-btn" 
-        onClick={handleDeco}
-                >Deconnection
-        </button>
-        {posts.post.map((element: { id: any; }) => (
-            <Post key={element.id} props={{element}}/>
-        ))}
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>Hello {username}</h1>
-        <button className="deco-btn" 
-        onClick={handleDeco}
-                >Deconnection
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Hello {username}</h1>
+      <button className="deco-btn" 
+      onClick={handleDeco}
+              >Deconnection
+      </button>
+    </div>
+  )
 
 }   
